a6ba36035aa9b0946bebdcdb54e35a8c99f274f1
narayan pravin deshpande <narayandeshpande92@gmail.com>
2024-11-16 22:16:37 +0530

    final

--- Backend/Frontend/src/context/SocketContaxt.jsx
+++ Backend/Frontend/src/context/SocketContaxt.jsx
@@ -13,7 +13,7 @@ export const SocketProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const newSocket = io("http://localhost:3000", {
+            const newSocket = io("https://chatapp-bnvo.onrender.com", {
                 query: {
                     userId: authUser.user.id,
                 },
