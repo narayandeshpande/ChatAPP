7cd34527bd230c111b8887ee342a03e67f8f9c1f
narayan pravin deshpande <102522755+narayandeshpande@users.noreply.github.com>
2025-01-17 09:34:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieparser())
 app.use(cors(
         {
-                origin: '*',
+                origin: 'https://marvelous-churros-168246.netlify.app',
                 credentials:true
         }
 ))
