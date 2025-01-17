72cc6e376597e8f5270135b5a035b095dc14f8f6
narayan pravin deshpande <102522755+narayandeshpande@users.noreply.github.com>
2025-01-17 09:30:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieparser())
 app.use(cors(
         {
-                origin: *,
+                origin: '*',
                 credentials:true
         }
 ))
