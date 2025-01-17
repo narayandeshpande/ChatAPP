05564f3ab99fb46f23a5bc7b64b9f53b2ba17b0a
narayan pravin deshpande <102522755+narayandeshpande@users.noreply.github.com>
2025-01-17 09:45:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieparser())
 app.use(cors(
         {
-                origin: 'https://chatapp-narayan.netlify.app,
+                origin: 'https://chatapp-narayan.netlify.app',
                 credentials:true
         }
 ))
